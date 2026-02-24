cc655364ae702e1bbd98f5a3c77a9a791b660cf4
hichri-louay <louay.hichri@hotmail.com>
2026-02-25 00:39:42 +0100

    add configuration cors , helmet , hpp

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
-const app = express();
-
 const path = require("path");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const chainlistRoutes = require("./routes/chainlist.routes");
 const {
@@ -15,20 +16,24 @@ const {
 
 require("dotenv").config();
 
+const app = express();
+
+app.use(helmet());
+app.use(hpp());
+app.use(cors({ origin: "*", methods: ["GET"] }));
+
+app.use(morgan("combined"));
+
 app.use(rateLimiterMiddleware);
 
 app.use(express.static(path.resolve("public")));
 
-app.use(express.json());
-
 app.use("/chainlist", chainlistRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`),
 );
